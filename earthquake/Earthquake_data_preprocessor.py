285d5bd55ead2c895e5c74a99f8cdff97e0f5115
sanmaoyang <58927287+sanmaoyang@users.noreply.github.com>
2024-02-15 11:31:58 +0800

    Add files via upload

---
+++ earthquake/Earthquake_data_preprocessor.py
@@ -0,0 +1,110 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import Binarizer
+
+class EarthquakeDataPreprocessor:
+
+  """
+  A class for preprocessing earthquake prediction data.
+
+  Args:
+    data: The input data.
+    remove_nan: Whether to remove NaN values.
+    remove_duplicates: Whether to remove duplicate rows.
+    binarize_magnitude: Whether to binarize the magnitude column.
+    threshold: The threshold for binarization.
+    test_size: The size of the test set.
+    validation_size: The size of the validation set.
+    n: The number of rows to remove between the training and testing sets.
+    normalize_data: Whether to normalize the data.
+  """
+
+  def __init__(self, data, remove_nan=False, remove_duplicates=False, binarize_magnitude=True, threshold=5, test_size=0.3, validation_size=0.3, n=50, normalize_data=False):
+    self.data = data
+    self.remove_nan = remove_nan
+    self.remove_duplicates = remove_duplicates
+    self.binarize_magnitude = binarize_magnitude
+    self.threshold = threshold
+    self.test_size = test_size
+    self.validation_size = validation_size
+    self.n = n
+    self.normalize_data = normalize_data
+
+  def _remove_nan(self):
+    """
+    Remove NaN values from the data.
+    """
+
+    if self.remove_nan:
+      self.data.dropna(axis=0, inplace=True)
+
+  def _remove_duplicates(self):
+    """
+    Remove duplicate rows from the data.
+    """
+
+    if self.remove_duplicates:
+      self.data.drop_duplicates(inplace=True)
+
+  def _binarize_magnitude(self):
+    """
+    Binarize the magnitude column.
+    """
+
+    if self.binarize_magnitude:
+      self.data.iloc[:, -1] = self.data.iloc[:, -1] >= self.threshold
+
+  def _split_train_test(self):
+    """
+    Split the data into training and testing sets.
+    """
+
+    train_data, test_data = train_test_split(self.data, test_size=self.test_size, shuffle=False)
+    return train_data[:-50], test_data
+
+  def _split_train_validation(self, train_data):
+    """
+    Split the training data into training and validation sets.
+    """
+
+    train_data, validation_data = train_test_split(train_data, test_size=self.validation_size,shuffle=False)
+    return train_data[:-50], validation_data
+
+  def _normalize_data(self, train_data):
+    """
+    Normalize the data.
+    """
+
+    if self.normalize_data:
+      mean = train_data.mean()
+      std = train_data.std()
+      self.data = (self.data - mean) / std
+
+  def get_datasets(self):
+    """
+    Get the training, validation, and testing sets.
+
+    Returns:
+      The training, validation, and testing sets.
+    """
+
+    # Remove NaN values
+    self._remove_nan()
+
+    # Remove duplicate rows
+    self._remove_duplicates()
+
+    # Binarize the magnitude column
+    self._binarize_magnitude()
+
+    # Split the data into training and testing sets
+    train_data, test_data = self._split_train_test()
+    # Normalize the data
+    self._normalize_data(train_data)
+    # Split the data into training and testing sets again
+    train_data, test_data = self._split_train_test()
+    # Split the training data into training and validation sets
+    train_data, validation_data = self._split_train_validation(train_data)
+
+
+    return train_data, validation_data, test_data
---
+++ earthquake/Model_Tuner.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import PredefinedSplit, GridSearchCV
+from sklearn.metrics import f1_score, roc_auc_score, matthews_corrcoef
+class ModelTuner:
+
+    def __init__(self, model, train_data, validation_data, test_data):
+        self.model = model
+        self.train_data = train_data
+        self.validation_data = validation_data
+        self.test_data = test_data
+        self.best_parameters = None
+        self.mcc_index = 1
+        self.y_pred = None
+        self.y_pro = None
+
+
+    def tune(self,fixed_params,param_grid):
+        # Combine the train and validation data
+        train = pd.concat([self.train_data, self.validation_data])
+
+        # Create a predefined split
+        split_index = [-1 if x in self.validation_data.index else 0 for x in self.train_data.index]
+        pds = PredefinedSplit(test_fold=split_index)
+
+        # Set the fixed parameters in the model
+        self.model.set_params(**fixed_params)
+
+        # Create a grid search CV object
+        clf = GridSearchCV(estimator=self.model, cv=pds, param_grid=param_grid)
+
+        # Fit the model to the training data
+        clf.fit(train.iloc[:,:-1], train.iloc[:,-1])
+
+        # Get the best parameters
+        self.best_parameters = clf.best_params_
+        self.model.set_params(**self.best_parameters)
+        self.model.set_params(**fixed_params)
+
+        return self.best_parameters , self.model
+
+    def mcc_index(self):
+        self.model.fit(self.train_data.iloc[:,:-1], self.train_data.iloc[:,-1])
+        # Predict the labels on the validation data
+        y_pred = self.model.predict(self.validation_data.iloc[:,:-1])
+        # Calculate the MCC
+        mcc_value = matthews_corrcoef(self.validation_data.iloc[:,-1], y_pred)
+        if mcc_value < 0:
+            self.mcc_index = 0
+    
+    def getpredict(self):
+        train = pd.concat([self.train_data, self.validation_data])
+        self.model.fit(train.iloc[:,:-1], train.iloc[:,-1])
+        self.y_pred = self.model.predict(self.test_data.iloc[:,:-1])
+        self.y_pro = self.model.predict_proba(self.test_data.iloc[:,:-1])[:,0]
+
+    def evaluate(self,mccmax = False):
+        self.getpredict()
+        # Calculate F1 score
+        f1 = f1_score(self.test_data.iloc[:,-1], self.y_pred)
+        # Calculate AUC
+        auc = roc_auc_score(self.test_data.iloc[:,-1], self.y_pro)
+        # Calculate MCC
+        mcc = matthews_corrcoef(self.test_data.iloc[:,-1], self.y_pred)
+        # Calculate recall
+        # Create a DataFrame
+        results = pd.DataFrame({
+            "F1": [f1],
+            "AUC": [auc],
+            "MCC": [mcc],
+        })
+        return results 
